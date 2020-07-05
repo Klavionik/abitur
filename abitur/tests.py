cc62a7da93ef26972fb2dc9d649162a0b3608f35
Roman Vlasenko <klavionik@gmail.com>
2020-07-05 18:13:00 +0500

    test: refactoring, more test cases

--- abitur/tests.py
+++ abitur/tests.py
@@ -1,7 +1,7 @@
 import os
-from unittest import TestCase
+from unittest import TestCase, TestSuite
 from unittest.mock import patch, MagicMock
-from abitur.parsers import PirogovaParser
+from abitur.parsers import PirogovaParser, SechenovaParser, SechenovaBVIParser
 from django.conf import settings
 from json import load
 
@@ -13,23 +13,30 @@ def mock_pdf(data):
     return mock
 
 
-class PirogovaParserTest(TestCase):
-    file_path = os.path.join(settings.BASE_DIR, 'abitur', 'fixtures', 'pirogova_list.pdf')
-    page_path = os.path.join(settings.BASE_DIR, 'abitur', 'fixtures', 'pirogova_page.html')
-    data_path = os.path.join(settings.BASE_DIR, 'abitur', 'fixtures', 'pirogova_data.json')
-    source_url = 'http://rsmu.ru/fileadmin/rsmu/img/abiturients/2020/03_07_2020_biologija.pdf'
+class ParserTestCase(TestCase):
+    app = 'abitur'
+    fixtures_dir = 'fixtures'
+    file_name = ''
+    page_name = ''
+    data_name = ''
+    parser_class = None
+    expected_source = ''
 
     @classmethod
     def setUpClass(cls):
-        cls.parser = PirogovaParser()
+        file_path = os.path.join(settings.BASE_DIR, cls.app, cls.fixtures_dir, cls.file_name)
+        page_path = os.path.join(settings.BASE_DIR, cls.app, cls.fixtures_dir, cls.page_name)
+        data_path = os.path.join(settings.BASE_DIR, cls.app, cls.fixtures_dir, cls.data_name)
 
-        with open(cls.file_path, 'rb') as file:
+        cls.parser = cls.parser_class()
+
+        with open(file_path, 'rb') as file:
             cls.file = file.read()
 
-        with open(cls.page_path, 'rb') as page:
+        with open(page_path, 'rb') as page:
             cls.page = page.read()
 
-        with open(cls.data_path) as data:
+        with open(data_path) as data:
             cls.data = load(data)
 
         cls.parser._page = cls.page
@@ -37,7 +44,9 @@ class PirogovaParserTest(TestCase):
     def test_get_source(self):
         self.parser.get_source()
 
-        self.assertEqual(self.parser.source_url, self.source_url)
+        self.assertEqual(
+            self.parser.source_url, self.expected_source, 'get_source returns correct source url'
+        )
 
     @patch('abitur.parsers.PirogovaParser.get_source')
     @patch('abitur.parsers.read_pdf')
@@ -45,17 +54,72 @@ class PirogovaParserTest(TestCase):
         mock_read_pdf.return_value = self.file
         file = self.parser.get_file()
 
-        self.assertEqual(file, self.file)
+        self.assertEqual(file, self.file, 'get_file returns a file produced by read_pdf')
+
+
+class PirogovaParserTest(ParserTestCase):
+    file_name = 'pirogova_list.pdf'
+    page_name = 'pirogova_page.html'
+    data_name = 'pirogova_data.json'
+    parser_class = PirogovaParser
+    expected_source = 'http://rsmu.ru/fileadmin/rsmu/img/abiturients/2020/03_07_2020_biologija.pdf'
 
     def test_parse_tables(self):
         pdf_mock = mock_pdf(self.data)
         self.parser.parse_tables(pdf_mock)
-        expected_total = 62
         expected_bvi = 1
         expected_funded_only = 27
         expected_others = 34
+        expected_total = expected_bvi + expected_others + expected_funded_only
 
         self.assertEqual(len(self.parser), expected_total)
-        self.assertEqual(len(self.parser.categories[0]), expected_bvi)
-        self.assertEqual(len(self.parser.categories[1]), expected_funded_only)
-        self.assertEqual(len(self.parser.categories[2]), expected_others)
+        self.assertEqual(len(self.parser.categories['bvi']), expected_bvi)
+        self.assertEqual(len(self.parser.categories['funded_only']), expected_funded_only)
+        self.assertEqual(len(self.parser.categories['general']), expected_others)
+
+
+class SechenovaParserTest(ParserTestCase):
+    file_name = 'sechenova_list.pdf'
+    page_name = 'sechenova_page.html'
+    data_name = 'sechenova_data.json'
+    parser_class = SechenovaParser
+    expected_source = 'https://www.sechenov.ru/upload/iblock/d62/' \
+                      'Bakalavriat_-spetsialitet-_-spisok-lits-podavshikh-dokumenty.pdf'
+
+    def test_parse_tables(self):
+        pdf_mock = mock_pdf(self.data)
+        self.parser.parse_tables(pdf_mock)
+        expected_funded_only = 28
+        expected_others = 46
+        expected_total = expected_others + expected_funded_only
+
+        self.assertEqual(len(self.parser), expected_total)
+        self.assertEqual(len(self.parser.categories['funded_only']), expected_funded_only)
+        self.assertEqual(len(self.parser.categories['general']), expected_others)
+
+
+class SechenovaBVIParserTest(ParserTestCase):
+    file_name = 'sechenova_bvi_list.pdf'
+    page_name = 'sechenova_page.html'
+    data_name = 'sechenova_bvi_data.json'
+    parser_class = SechenovaBVIParser
+    expected_source = 'https://www.sechenov.ru/upload/iblock/5b9/' \
+               'Bakalavriat_-spetsialitet-_-spisok-lits-podavshikh-dokumenty-bez-VI.pdf'
+
+    def test_parse_tables(self):
+        pdf_mock = mock_pdf(self.data)
+        self.parser.parse_tables(pdf_mock)
+        expected_total = 0
+
+        self.assertEqual(len(self.parser), expected_total)
+
+
+test_cases = (PirogovaParserTest, SechenovaParserTest, SechenovaBVIParserTest)
+
+
+def load_tests(loader, tests, pattern):
+    suite = TestSuite()
+    for test_case in test_cases:
+        tests = loader.loadTestsFromTestCase(test_case)
+        suite.addTests(tests)
+    return suite
